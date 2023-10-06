8702af8309879c38ed880b9462e45502cbfa31a4
josemeloph <contato.josemeloph@gmail.com>
2023-10-06 13:57:35 -0300

    gerador de senhas

---
+++ GeradorDeSenhas/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using GeradorDeSenhas.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeradorDeSenhas.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(Senha senha)
+        {
+            return View(senha);
+        }
+
+        [HttpPost]
+        public IActionResult GerarSenha(Senha senha)
+        {
+            string caracteres = senha.CaracteresPossiveis();
+            Random sorteio = new Random();
+            int numeroSorteado = 0;
+            StringBuilder senhaGerada = new StringBuilder();
+
+            if (caracteres.Length > 0)
+            {
+                for (int i = 0; i < senha.Tamanho; i++)
+                {
+                    numeroSorteado = sorteio.Next(0, caracteres.Length - 1);
+                    senhaGerada.Append(caracteres[numeroSorteado]);
+                }
+            }
+            senha.Valor = senhaGerada.ToString();
+            return RedirectToAction("Index", senha);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ GeradorDeSenhas/Models/Senha.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Policy;
+using System.Text;
+
+namespace GeradorDeSenhas.Models
+{
+    public class Senha
+    {
+        public string Valor {  get; set; }
+        public int Tamanho {  get; set; }
+        public bool Maiusculas {  get; set; }
+        public bool Minusculas { get; set; }
+        public bool Numeros {  get; set; }
+        public bool Especiais { get; set; }
+        
+        public string CaracteresPossiveis()
+        {
+            StringBuilder caracteresPossiveis = new StringBuilder();
+            if (Maiusculas) caracteresPossiveis.Append("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+            if (Minusculas) caracteresPossiveis.Append("abcdefghijklmnopqrstuvwxyz");
+            if (Numeros) caracteresPossiveis.Append("1234567890");
+            if (Especiais) caracteresPossiveis.Append("!@#$%&*()=-+.,;?{}[]^><:");
+            return caracteresPossiveis.ToString();
+        }
+
+    }
+}
